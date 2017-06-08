d74c3f09da7370846631991884347a2a5856072f
unknown <xconstaud@gmail.com>
2017-06-08 10:59:30 -0500

    Implement name, create flush function

--- AgeTest/main.c
+++ AgeTest/main.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
+void flushInput() {
+	int ch;
+	while ((ch = getchar()) != '\n' && ch != EOF);
+}
+
 int main()
 {
 	char ageString[10];
+	char firstName[15];
+	char lastName[15];
 	int age = 70;
 	double bonus;
+
+	// Ask user for name
+	printf("What is your first name? \n");
+	fgets(firstName, 15, stdin);
+	
+	printf("What is your last name? \n");
+	fgets(lastName, 15, stdin);
 
 	// Ask user for age
 	printf("What is your age? ");
 	fgets(ageString, 10, stdin);
 	age = atoi(ageString);
+	flushInput();
 
 	// Calculate the bonus a person based off age
 	if (age == 0) {
@@ -23,7 +38,8 @@ int main()
 			bonus = 2500.50;
 		}
 	}
-
+	
+	printf("Hello %s %s", firstName, lastName);
 	printf("Because you are %d years of age, your bonus is: $%.2f\n", age, bonus);
 
 	return 0;
