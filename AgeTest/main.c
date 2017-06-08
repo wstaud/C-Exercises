0f87cb6c41768a3072d445f8e1249530f65959a1
unknown <xconstaud@gmail.com>
2017-06-08 10:06:31 -0500

    Update ageTest with user input

--- AgeTest/main.c
+++ AgeTest/main.c
@@ -2,9 +2,16 @@
 
 int main()
 {
+	char ageString[10];
 	int age = 70;
 	double bonus;
 
+	// Ask user for age
+	printf("What is your age? ");
+	gets(ageString);
+	age = atoi(ageString);
+
+	// Calculate the bonus a person based off age
 	if (age > 40) {
 		bonus = 5000.00;
 	}
