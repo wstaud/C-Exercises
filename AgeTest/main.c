5b44801e3ba547e8d9051103705f9e6cdcd81ab2
unknown <xconstaud@gmail.com>
2017-06-08 10:15:57 -0500

    Implement fgets, replace gets()

--- AgeTest/main.c
+++ AgeTest/main.c
@@ -8,15 +8,20 @@ int main()
 
 	// Ask user for age
 	printf("What is your age? ");
-	gets(ageString);
+	fgets(ageString, 10, stdin);
 	age = atoi(ageString);
 
 	// Calculate the bonus a person based off age
-	if (age > 40) {
-		bonus = 5000.00;
+	if (age == 0) {
+		printf("You entered an invalid age!");
 	}
 	else {
-		bonus = 2500.50;
+		if (age > 40) {
+			bonus = 5000.00;
+		}
+		else {
+			bonus = 2500.50;
+		}
 	}
 
 	printf("Because you are %d years of age, your bonus is: $%.2f\n", age, bonus);
