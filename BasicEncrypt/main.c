844a128caf45330d785a44afec6220d0ce6e2d90
unknown <xconstaud@gmail.com>
2017-06-09 10:52:45 -0500

    Update Encryption with functional encryption, Update .gitignore to remove debug stuff.

--- BasicEncrypt/main.c
+++ BasicEncrypt/main.c
@@ -1,18 +1,50 @@
 #include <stdio.h>
 
 // Input Buffer Flush
-void flushInput() {
-	int ch;
-	while ((ch = getchar()) != '\n' && ch != EOF);
+int readln(char s[], int maxlen) {
+	char ch;
+	int i;
+	int chars_remain;
+
+	i = 0;
+	chars_remain = 1;
+	while (chars_remain) {
+		ch = getchar();
+		if ((ch == '\n') || (ch == EOF)) {
+			chars_remain = 0;
+		}
+		else if (i < maxlen - 1) {
+			s[i] = ch;
+			i++;
+		}
+	}
+	s[i] = '\0';
+	return i;
 }
 
 // Encrypt Passpharse
 void encrypt() {
-	printf("Encrypt");
+	char passpharse[50];
+	char encryptedPasspharse[50];
+
+	printf("Encryption Selected.\n");
+	printf("What is the passpharse you would like to encrypt?\n");
+	readln(passpharse, 50);
+	
+	for (int i = 0; i < 50; i++) {
+		if (passpharse[i] == '\0') {
+			encryptedPasspharse[i] = '\0';
+			printf("BREAK");
+			break;
+		}
+		encryptedPasspharse[i] = passpharse[i] + 1;
+		//debug printf("%c", passpharse[i]);
+	}
+	printf("Encrypted passpharse is: %s", encryptedPasspharse);
 }
 
 void decrypt() {
-	printf("Decrypt");
+	printf("Decryption Selected.\n");
 }
 
 // Prompt User
@@ -25,19 +57,16 @@ void promptUser() {
 	// Ask user if they want to encrypt or decrypt
 	printf("1: Encrypt a passpharse\n");
 	printf("2: Decrypt a passpharse\n");
-	fgets(menuSelection, 2, stdin);
+	readln(menuSelection, 2);
 	selection = atoi(menuSelection);
 
 	if (selection == 1) {
-		flushInput();
 		encrypt();
 	}
 	else if (selection == 2) {
-		flushInput();
 		decrypt();
 	}
 	else {
-		flushInput();
 		printf("invalid menu selection");
 		promptUser();
 	}
